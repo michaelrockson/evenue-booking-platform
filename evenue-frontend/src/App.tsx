a8a57ec0591b53d64b7bfc2f02c455ede987f736
Michael Rockson <michaelrockszn@gmail.com>
2026-07-26 11:31:46 +0000

    chore(frontend): initial commint

---
+++ evenue-frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button"
+import './App.css'
+
+
+function App() {
+
+  return (
+    <Button>
+      <div>Click Me</div>
+    </Button>
+  )
+}
+
+export default App
